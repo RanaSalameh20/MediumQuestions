d3c1585ef84b86937ec7325adcf0796c181f4e9e
RanaSalameh <rana.salameh313@gmail.com>
2023-04-25 19:18:51 +0300

    solve Permutation question

---
+++ Permutations/Permutation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Permutations
+{
+    internal class Permutation
+    {
+        public IList<IList<int>> FindPermutations(int[] nums)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            Permute(nums, 0, result);
+            return result;
+        }
+
+        public void Permute(int[] nums, int start, IList<IList<int>> result)
+        {
+            if (start == nums.Length - 1)
+            {
+                IList<int> permutation = new List<int>(nums);
+                result.Add(permutation);
+                return;
+            }
+
+            for (int i = start; i < nums.Length; i++)
+            {
+                Swap(nums, start, i);
+                Permute(nums, start + 1, result);
+                Swap(nums, start, i);
+            }
+        }
+
+        public void Swap(int[] nums, int i, int j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        internal void PrintPermutations(IList<IList<int>> permutations)
+        {
+            Console.Write("[");
+            foreach (IList<int> permutation in permutations)
+            {
+                Console.Write("[");
+                for (int i = 0; i < permutation.Count; i++)
+                {
+                    Console.Write(permutation[i]);
+                    if (i != permutation.Count - 1)
+                    {
+                        Console.Write(",");
+                    }
+                }
+                Console.Write("]");
+                //Console.WriteLine();
+            }
+            Console.Write("]");
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ Permutations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permutations
+{
+    class Program
+    {
+  
+        static void Main(string[] args)
+        {
+            //case 1 
+            Permutation permutationObject = new Permutation();
+            int[] nums = { 1, 2, 3 };
+            IList<IList<int>> permutations = permutationObject.FindPermutations(nums);
+            permutationObject.PrintPermutations(permutations);
+
+            
+
+            //Case 2
+            int[] nums1 = { 0, 1 };
+            IList<IList<int>> permutations1 = permutationObject.FindPermutations(nums1);
+            permutationObject.PrintPermutations(permutations1);
+
+         
+
+            //case 3 
+            int[] nums2 = { 1 };
+            IList<IList<int>> permutations2 = permutationObject.FindPermutations(nums2);
+            permutationObject.PrintPermutations(permutations2);
+
+        }
+    }
+}
